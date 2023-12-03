5703a89abbd2bcf8ed340f5f6b5868fe65ce8a40
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-12-02 20:05:05 -0500

    bare minimum integration

--- wordserweb/internal/handlers/dashboard.go
+++ wordserweb/internal/handlers/dashboard.go
@@ -5,25 +5,146 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/labstack/echo/v4"
 )
 
-// TODO: Remove me. More hacks for homework
-type SynonymsResp struct {
-	Synonyms []string `json:"synonymns"`
+type TranslateLanguage string
+
+const (
+	Arabic    TranslateLanguage = "ar"
+	Chinese   TranslateLanguage = "zh"
+	English   TranslateLanguage = "en"
+	French    TranslateLanguage = "fr"
+	German    TranslateLanguage = "de"
+	Greek     TranslateLanguage = "el"
+	Italian   TranslateLanguage = "it"
+	Portugese TranslateLanguage = "pt"
+	Spanish   TranslateLanguage = "es"
+	Russian   TranslateLanguage = "ru"
+)
+
+func (t TranslateLanguage) String() string {
+	return string(t)
+}
+
+func (t TranslateLanguage) ToLanguageName() (string, error) {
+	switch t {
+	case Arabic:
+		return "Arabic", nil
+	case Chinese:
+		return "Chinese", nil
+	case English:
+		return "English", nil
+	case French:
+		return "French", nil
+	case German:
+		return "German", nil
+	case Greek:
+		return "Greek", nil
+	case Italian:
+		return "Italian", nil
+	case Portugese:
+		return "Portugese", nil
+	case Spanish:
+		return "Spanish", nil
+	case Russian:
+		return "Russian", nil
+	default:
+		return "", fmt.Errorf(
+			"invalid parameters: %v; source-language not supported;",
+			t,
+		)
+	}
+}
+
+type TranslateResp struct {
+	TranslatedText string `json:"translated_text"`
 }
 
 func GetDashboardHandler(c echo.Context) error {
-	// TODO: remove me
-	resp, err := http.Get("http://wordser:8080/api/v1/synonyms?word=friend")
+	return c.Render(http.StatusOK, "dashboard", "")
+}
+
+type GetTranslateHandlerReq struct {
+	TranslateText  string            `query:"translate-text"`
+	SourceLanguage TranslateLanguage `query:"source-language"`
+	TargetLanguage TranslateLanguage `query:"target-language"`
+}
+
+func GetTranslateHandler(c echo.Context) error {
+	var params GetTranslateHandlerReq
+	err := c.Bind(&params)
+	if err != nil {
+		return c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf(
+				"invalid parameters: %v",
+				params,
+			),
+		)
+	}
+
+	if params.TranslateText == "" {
+		return c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf(
+				"invalid parameters: %v, translate-text can't be empty;",
+				params,
+			),
+		)
+	}
+
+	sourceLangName, err := params.SourceLanguage.ToLanguageName()
+	if err != nil {
+		return c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf(
+				"invalid parameters: %v; source-language not supported;",
+				params,
+			),
+		)
+	}
+
+	targLangName, err := params.TargetLanguage.ToLanguageName()
+	if err != nil {
+		return c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf(
+				"invalid parameters: %v; target-language not supported;",
+				params,
+			),
+		)
+	}
+
+	if params.SourceLanguage == params.TargetLanguage {
+		return c.String(
+			http.StatusBadRequest,
+			fmt.Sprintf(
+				"invalid parameters: %v; support-language cannot equal target-language;",
+				params,
+			),
+		)
+	}
+
+	// TODO: remove me. this is partner's micorservice.
+	// after course just use wordser translate
+	resp, err := http.Get(
+		fmt.Sprintf(
+			"http://backend:5000/translate?inputText=%s&sourceLanguage=%s&targetLanguage=%s",
+			url.QueryEscape(params.TranslateText),
+			url.QueryEscape(params.SourceLanguage.String()),
+			url.QueryEscape(params.TargetLanguage.String()),
+		),
+	)
 	fmt.Printf("\n\n resp: %v, err: %v \n\n", resp, err)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to get synonyms from wordser; statusCode: %d", resp.StatusCode)
+		return fmt.Errorf("failed to get translation from partner's backend; statusCode: %d", resp.StatusCode)
 	}
 
 	data, err := io.ReadAll(resp.Body)
@@ -31,10 +152,26 @@ func GetDashboardHandler(c echo.Context) error {
 		return err
 	}
 
-	syns := &SynonymsResp{}
-	json.Unmarshal(data, syns)
+	transResp := &TranslateResp{}
+	json.Unmarshal(data, transResp)
 
-	fmt.Printf("\n synonymns: %v \n", syns)
+	fmt.Printf("\n translated text: %v \n", transResp)
 
-	return c.Render(http.StatusOK, "dashboard", "")
+	return c.HTML(
+		http.StatusOK,
+		fmt.Sprintf(
+			`
+			<div>
+				<p>Source Language: %s</p>
+				<p>Source Text: %s</p>
+				<p>Translated Language: %s</p>
+				<p>Translated Text: %s</p>
+			</div>
+			`,
+			sourceLangName,
+			params.TranslateText,
+			targLangName,
+			transResp.TranslatedText,
+		),
+	)
 }
